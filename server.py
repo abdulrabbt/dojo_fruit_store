c7eb58aea6298312601121825691afab8c7d4edb
abdulrab <abdulrab.bt@hotmail.com>
2019-09-09 12:37:01 +0300

    done

--- server.py
+++ server.py
@@ -8,7 +8,18 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    strawberry=request.form['strawberry']
+    raspberry=request.form['raspberry']
+    apple=request.form['apple']
+    blackberry=request.form['blackberry']
+
+    first_name=request.form['first_name']
+    last_name=request.form['last_name']
+    student_id=request.form['student_id']
+    sum=int(strawberry)+int(raspberry)+int(apple)+int(blackberry)
+    
+    chargin="Charging "+ first_name+ " " +last_name +":  " +str(sum)+" fruits"
+    return render_template("checkout.html",blackberry=blackberry,apple=apple,raspberry=raspberry,strawberry=strawberry,first_name=first_name,last_name=last_name,student_id=student_id,chargin=chargin)
 
 @app.route('/fruits')         
 def fruits():
